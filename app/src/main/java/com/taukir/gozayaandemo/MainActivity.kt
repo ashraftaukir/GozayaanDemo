faee85f5ce5650e6c0b0e5d86b3ee46940d36fce
taukir48 <ashraftaukir@gmail.com>
2024-11-15 23:45:10 +0600

    initial commit

---
+++ app/src/main/java/com/taukir/gozayaandemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.taukir.gozayaandemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
